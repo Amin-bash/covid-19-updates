382929c2d68ca5ca1d1d965486bf257ef64e383e
amin-bash <power.ameen99@gmail.com>
2020-06-26 18:17:57 +0300

    Updates

---
+++ precache-manifest.921198f0e0c4dc0ddbd0f3f117620975.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc7ba4dbffd092978903440d218c061",
+    "url": "/covid-19-updates/index.html"
+  },
+  {
+    "revision": "f188a47a8f56ff990d22",
+    "url": "/covid-19-updates/static/css/main.06f5db4c.chunk.css"
+  },
+  {
+    "revision": "19e04db743d0be42c9d0",
+    "url": "/covid-19-updates/static/js/2.10b44180.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-updates/static/js/2.10b44180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f188a47a8f56ff990d22",
+    "url": "/covid-19-updates/static/js/main.2d8ac7c1.chunk.js"
+  },
+  {
+    "revision": "77024d8a33f186d35798",
+    "url": "/covid-19-updates/static/js/runtime-main.382c88bb.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-updates/static/media/image.d7265326.png"
+  }
+]);
